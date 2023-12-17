e348bafa08b3e2e9785d9476c99a098d56e7e54b
ankit kashyap <ankitankitkashyap45@gmail.com>
2023-12-17 13:48:11 +0530

    PWA added.

--- vite.config.js
+++ vite.config.js
@@ -3,49 +3,59 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa"
 
 const manifestForPlugIn = {
-  registerType:'prompt',
-  includeAssests:['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
-  manifest:{
-    name:"React-vite-app",
-    short_name:"react-vite-app",
-    description:"I am a simple vite app",
-    icons:[{
+  registerType: 'prompt',
+  includeAssests: ['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Tech2radar AI",
+    short_name: "tech2radar-ai",
+    description: "Empower your creativity with free AI tools! AI Toolbox offers a suite of powerful artificial intelligence tools to enhance your projects. From image recognition to natural language processing, unleash the potential of AI without any cost.",
+    icons: [{
       src: '/android-chrome-192x192.png',
-      sizes:'192x192',
-      type:'image/png',
-      purpose:'favicon'
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'favicon'
     },
     {
-      src:'/android-chrome-512x512.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'favicon'
+      src: '/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'favicon'
     },
     {
       src: '/apple-touch-icon.png',
-      sizes:'180x180',
-      type:'image/png',
-      purpose:'apple touch icon',
+      sizes: '180x180',
+      type: 'image/png',
+      purpose: 'apple touch icon',
     },
     {
-      src: '/maskable_icon.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'any maskable',
+      src: '/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any maskable',
     }
-  ],
-  theme_color:'#171717',
-  background_color:'#f0e7db',
-  display:"standalone",
-  scope:'/',
-  start_url:"/",
-  orientation:'portrait'
-  }
+    ],
+    splash_screen: {
+      show: true,
+      image: "/android-chrome-512x512.png",
+      sizes: "512x512",
+      background_color: "#ffffff"
+    },
+    theme_color: '#6366f1',
+    background_color: '#ffffff',
+    display: "standalone",
+    scope: '/',
+    start_url: "/",
+    orientation: 'portrait'
+  },
+  // devOptions: {
+  //   enabled: true
+  //   /* other options */
+  // }
 }
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),VitePWA(manifestForPlugIn)],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   moduleContext: {
     './node_modules/pdfmake/build/vfs_fonts.js': 'window',
-  },  
+  },
 })
